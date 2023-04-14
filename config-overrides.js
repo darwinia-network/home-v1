6892fa95b6e1a8db12cd15fc4982529cd2e4ec77
JayJay1024 <lygm1062@gmail.com>
2023-04-14 16:59:19 +0800

    chore: update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,30 +1,30 @@
-const PrerenderSPAPlugin = require('prerender-spa-plugin');
-const path = require('path');
+// const PrerenderSPAPlugin = require('prerender-spa-plugin');
+// const path = require('path');
 
 module.exports = (config, env) => {
-  if (env === 'production') {
-    config.plugins = config.plugins.concat([
-      new PrerenderSPAPlugin({
-        routes: [
-          '/home-v1/',
-          '/home-v1/faq',
-          '/home-v1/brand',
-          '/home-v1/ambassador',
-          '/home-v1/community',
-          '/home-v1/blog',
-          '/home-v1/media',
-          '/home-v1/reports',
-          '/home-v1/events',
-          '/home-v1/videos',
-          '/home-v1/news',
-          '/home-v1/tech',
-          '/home-v1/economic_model',
-          '/home-v1/404'
-        ],
-        staticDir: path.join(__dirname, 'build'),
-      }),
-    ]);
-  }
+  // if (env === 'production') {
+  //   config.plugins = config.plugins.concat([
+  //     new PrerenderSPAPlugin({
+  //       routes: [
+  //         '/home-v1/',
+  //         '/home-v1/faq',
+  //         '/home-v1/brand',
+  //         '/home-v1/ambassador',
+  //         '/home-v1/community',
+  //         '/home-v1/blog',
+  //         '/home-v1/media',
+  //         '/home-v1/reports',
+  //         '/home-v1/events',
+  //         '/home-v1/videos',
+  //         '/home-v1/news',
+  //         '/home-v1/tech',
+  //         '/home-v1/economic_model',
+  //         '/home-v1/404'
+  //       ],
+  //       staticDir: path.join(__dirname, 'build'),
+  //     }),
+  //   ]);
+  // }
 
   return config;
 };
