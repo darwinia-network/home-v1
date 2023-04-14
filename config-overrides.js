48e91c8e611ad31af9e107a28ca7d85a7dc966cf
JayJay1024 <lygm1062@gmail.com>
2023-04-14 17:42:18 +0800

    fix: failed to build

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,6 @@
 // const PrerenderSPAPlugin = require('prerender-spa-plugin');
 // const path = require('path');
+const webpack = require('webpack');
 
 module.exports = (config, env) => {
   // if (env === 'production') {
@@ -25,6 +26,22 @@ module.exports = (config, env) => {
   //     }),
   //   ]);
   // }
+  config.resolve.fallback = {
+    url: require.resolve('url'),
+    assert: require.resolve('assert/'),
+    crypto: require.resolve('crypto-browserify'),
+    http: require.resolve('stream-http'),
+    https: require.resolve('https-browserify'),
+    os: require.resolve('os-browserify/browser'),
+    buffer: require.resolve('buffer/'),
+    stream: require.resolve('stream-browserify'),
+  };
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+        process: 'process/browser',
+        Buffer: ['buffer', 'Buffer'],
+    }),
+  );
 
   return config;
 };
